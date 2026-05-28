96046fe93aa0380348c9b66717109d27fc1ea19d
Pascal <pascal.nugroho@gmail.com>
2026-05-29 03:34:43 +0700

    feat: bypass onboarding page and render satisfaction form directly on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,3 @@
-import Link from "next/link";
+import SatisfactionFormsPage from "./satisfaction-forms/page";
 
-export default function Home() {
-  return (
-    <main className="home-container">
-      <div className="home-card">
-        <img
-          src="/Assets/solustream_logo.png"
-          alt="Solustream Logo"
-          className="home-logo"
-        />
-        <h1 className="home-title">Feedback Portal</h1>
-        <p className="home-desc">
-          We strive to provide you with the best streaming service possible.
-          Please take a moment to fill out our satisfaction form and let us
-          know how we can improve.
-        </p>
-        <Link href="/satisfaction-forms" className="btn-start" id="btn-start-survey">
-          Start Survey
-          <span style={{ marginLeft: "8px", transition: "transform 0.2s ease" }}>&rarr;</span>
-        </Link>
-      </div>
-    </main>
-  );
-}
+export default SatisfactionFormsPage;
